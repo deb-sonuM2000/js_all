9017792f1474100e6c15fb4c7129f87d64d5e182
debadanmohanty <debadanmohanty7735199207@gmail.com>
2023-12-01 21:58:07 +0530

    Hii

---
+++ Loop.js
@@ -0,0 +1,25 @@
+/*let i=2;
+while (i<=10) {
+          console.log("WELCOME TO JAVASCRIPT", i);
+          i++
+}*/
+
+//do while loop
+let j=1
+do{
+          console.log("hww", j);
+          j++
+}while(j<=5)
+
+//for loop 
+/*for(let i=1;i<=5;i++){
+          console.log("hello js: ", i);
+          for(let j=1;j<=5;j++)
+          console.log("hii js: ", j);
+}*/
+
+for(let i=20;i<=100;i++){
+          if(i%3==0){
+                    console.log(i);
+          }
+}
