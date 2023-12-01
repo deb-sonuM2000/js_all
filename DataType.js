7a4d92f7013f3053bc965444510664bcef5dc82f
debadanmohanty <debadanmohanty7735199207@gmail.com>
2023-12-01 23:04:44 +0530

    Great

---
+++ DataType.js
@@ -0,0 +1,32 @@
+let data=8
+let user="Dev M"
+console.log(typeof user)
+console.log(typeof data)
+let num=5/0;
+console.log(num)
+let num1=-12/0;
+console.log(Number.MAX_VALUE*10)
+
+//String concat
+let fname="Sonu"
+let lname="Mohanty"
+let com=fname + " " +  lname
+console.log(com)
+
+let nme="Ram\tshyam "
+console.log(nme) 
+
+let bool=12>98
+console.log(typeof bool)
+
+let user1=null;
+console.log(user1)
+
+let user10 
+console.log(typeof user10)
+
+let num2=19 
+console.log(19/'sonu')
+
+let x=parseFloat("230SOnu")
+console.log(x)
